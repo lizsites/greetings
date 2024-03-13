a850924a848583c87f75cf11a4bf860222ffb476
lizsites <elizabeth.sites97@gmail.com>
2024-03-13 13:48:14 -0500

    adding random generation of greetings according to rand in math library

--- greetings.go
+++ greetings.go
@@ -3,14 +3,28 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
-	// Return a greeting that embeds the name in a message.
+	// rudimentary error handling call
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// returns a random string using the rand exported name from math
+func randomFormat() string {
+	formats := []string{
+		"Hey, %v! Welcome!",
+		"Great to see you, %v!",
+		"Ahoy, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
